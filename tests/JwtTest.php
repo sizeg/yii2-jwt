71b4364dea041c36dbac933c860b42de2b0e4412
Dmitry Demin <sizemail@gmail.com>
2020-12-24 12:37:17 +0500

    Removed deprecated methods from tests. Added newly methods used for feature 4.x version.

--- tests/JwtTest.php
+++ tests/JwtTest.php
@@ -2,6 +2,18 @@
 
 namespace sizeg\jwt\tests;
 
+use Lcobucci\Clock\FrozenClock;
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Token;
+use Lcobucci\JWT\Token\RegisteredClaims;
+use Lcobucci\JWT\Validation\Constraint\IdentifiedBy;
+use Lcobucci\JWT\Validation\Constraint\IssuedBy;
+use Lcobucci\JWT\Validation\Constraint\PermittedFor;
+use Lcobucci\JWT\Validation\Constraint\ValidAt;
+use sizeg\jwt\Jwt;
+
 class JwtTest extends TestCase
 {
 
@@ -9,17 +21,17 @@ class JwtTest extends TestCase
      * Secret key
      */
     const SECRET = 'secret';
-    
+
     /**
      * Issuer
      */
     const ISSUER = 'http://example.com';
-    
+
     /**
      * Audience
      */
     const AUDIENCE = 'http://example.org';
-    
+
     /**
      * Id
      */
@@ -35,44 +47,56 @@ class JwtTest extends TestCase
      */
     public function setUp()
     {
-        $this->jwt = \Yii::createObject(\sizeg\jwt\Jwt::class, [
+        $this->jwt = \Yii::createObject(Jwt::class, [
             ['key' => self::SECRET]
         ]);
     }
 
     /**
-     * @return Sha256 signer
+     * @return Signer
      */
-    public function getSignerSha256()
+    private function getSigner()
     {
-        return new \Lcobucci\JWT\Signer\Hmac\Sha256();
+        return $this->jwt->getSigner('HS256');
+    }
+
+    /**
+     * @return Key
+     */
+    private function getSigningKey()
+    {
+        return $this->jwt->getKey();
     }
 
     /**
      * @return Token created token
      */
-    public function createTokenWithSignature()
+    private function createTokenWithSignature()
     {
-        return $this->jwt->getBuilder()->setIssuer(self::ISSUER) // Configures the issuer (iss claim)
-                ->setAudience(self::AUDIENCE) // Configures the audience (aud claim)
-                ->setId(self::ID, true) // Configures the id (jti claim), replicating as a header item
-                ->setIssuedAt(time()) // Configures the time that the token was issue (iat claim)
-                ->setExpiration(time() + 3600) // Configures the expiration time of the token (nbf claim)
-                ->set('uid', 1) // Configures a new claim, called "uid"
-                ->sign($this->getSignerSha256(), $this->jwt->key) // creates a signature using "testing" as key
-                ->getToken(); // Retrieves the generated token
+        // creates a signature using "testing" as key
+        $signer = $this->getSigner();
+        $key = $this->getSigningKey();
+
+        $now = new \DateTimeImmutable();
+
+        return $this->getConfiguration()->builder()
+            ->issuedBy(self::ISSUER) // Configures the issuer (iss claim)
+            ->permittedFor(self::AUDIENCE) // Configures the audience (aud claim)
+            ->identifiedBy(self::ID) // Configures the id (jti claim), replicating as a header item
+            //->canOnlyBeUsedAfter($now->modify('+1 minute')) // Configures the time that the token can be used (nbf claim)
+            ->issuedAt($now) // Configures the time that the token was issue (iat claim)
+            ->expiresAt($now->modify('+1 hour')) // Configures the expiration time of the token (nbf claim)
+            ->withClaim('uid', 1) // Configures a new claim, called "uid"
+            ->withHeader('foo', 'bar') // Configures a new header, called "foo"
+            ->getToken($signer, $key); // Retrieves the generated token
     }
-    
+
     /**
-     * @return ValidationData
+     * @return Configuration
      */
-    public function getValidationData()
+    private function getConfiguration()
     {
-        $data = $this->jwt->getValidationData(); // It will use the current time to validate (iat, nbf and exp)
-        $data->setIssuer(self::ISSUER);
-        $data->setAudience(self::AUDIENCE);
-        $data->setId(self::ID);
-        return $data;
+        return $this->jwt->getConfiguratuionforSymmetricSigner($this->getSigner(), $this->getSigningKey());
     }
 
     /**
@@ -81,22 +105,39 @@ class JwtTest extends TestCase
     public function testValidateTokenWithSignature()
     {
         $token = $this->createTokenWithSignature();
-        $data = $this->getValidationData();
-        $is_verify = $token->verify($this->getSignerSha256(), $this->jwt->key);
-        $is_valid = $token->validate($data); // true, because validation information is equals to data contained on the token
-        $this->assertTrue($is_verify && $is_valid);
+        self::assertInstanceOf(Token\Plain::class, $token);
+
+        $configuration = $this->getConfiguration();
+        $configuration->setValidationConstraints(
+            new IdentifiedBy(self::ID),
+            new IssuedBy(self::ISSUER),
+            new PermittedFor(self::AUDIENCE)
+        );
+
+        $constraints = $configuration->validationConstraints();
+        self::assertTrue($configuration->validator()->validate($token, ...$constraints), 'Token validation failed');
     }
-    
+
     /**
      * Validate token timeout with signature
      */
-    public function testValidateTokenTimeoutWithSignature()
+    public function testValidateTokenTimeWithSignature()
     {
         $token = $this->createTokenWithSignature();
-        $data = $this->getValidationData();
-        $data->setCurrentTime(time() + 4000); // changing the validation time to future
-        $is_verify = $token->verify($this->getSignerSha256(), $this->jwt->key);
-        $is_valid = $token->validate($data); // false, because token is expired since current time is greater than exp
-        $this->assertFalse($is_verify && $is_valid);
+        self::assertInstanceOf(Token\Plain::class, $token);
+
+        $token_exp = $token->claims()->get(RegisteredClaims::EXPIRATION_TIME);
+        self::assertNotNull($token_exp);
+
+        // Changing the token exp time to future
+        $failed_clock = new FrozenClock($token_exp->modify('+1 minute'));
+
+        $configuration = $this->getConfiguration();
+        $configuration->setValidationConstraints(
+            new ValidAt($failed_clock)
+        );
+
+        $constraints = $configuration->validationConstraints();
+        self::assertFalse($configuration->validator()->validate($token, ...$constraints), 'Token time validation failed');
     }
 }
